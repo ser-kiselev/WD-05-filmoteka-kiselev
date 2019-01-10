cf015a187a59c1a672ec007fa1e66a549faa3fbc
Сергей Киселев <44898506+ser-kiselev@users.noreply.github.com>
2019-01-10 23:59:21 +0300

    first commit

---
+++ db/WD-05-filmoteka-kiselev.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Янв 10 2019 г., 23:55
+-- Версия сервера: 5.5.53
+-- Версия PHP: 5.6.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `WD-05-filmoteka-kiselev`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `films`
+--
+
+CREATE TABLE `films` (
+  `id` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `genre` text NOT NULL,
+  `year` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `films`
+--
+
+INSERT INTO `films` (`id`, `title`, `genre`, `year`) VALUES
+(1, 'Начало', 'фантастика, боевик', '2010'),
+(2, 'Гладиатор', 'боевик', '2000'),
+(3, 'Игры разума', 'драма', '2001'),
+(4, 'Интерстеллар', 'фантастика', '2014');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `films`
+--
+ALTER TABLE `films`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `films`
+--
+ALTER TABLE `films`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.php
@@ -0,0 +1,144 @@
+<?php 
+
+// Connection to DataBase
+$link = mysqli_connect('localhost', 'root', '', 'WD-05-filmoteka-kiselev');
+
+if ( mysqli_connect_error() ){
+	die("Ошибка подключения к базе данных");
+}
+
+// Save form data to DB
+$notifySuccess = "";
+$notifyError = "";
+$errors = array();
+
+if ( array_key_exists('newFilm', $_POST) ) {
+
+	// Error processing
+	if ( $_POST['title'] == '' ) {
+		$errors[] = "<p style='color: #fff;'>Название фильма не может быть пустым!</p>";
+	}
+
+	if ( $_POST['genre'] == '' ) {
+		$errors[] = "<p style='color: #fff;'>Введите жанр фильма!</p>";
+	}
+
+	if ( $_POST['year'] == '' ) {
+		$errors[] = "<p style='color: #fff;'>Введите год выхода фильма!</p>";
+	}
+
+	if ( empty($errors) ) {
+		// Writштп data to a DB
+		$query = "INSERT INTO films (title, genre, year) VALUES (
+	
+		'". mysqli_real_escape_string($link, $_POST['title']) ."', 
+		'". mysqli_real_escape_string($link, $_POST['genre']) ."', 
+		'". mysqli_real_escape_string($link, $_POST['year']) ."'
+		)";
+
+
+		if ( mysqli_query($link, $query) ) {
+			$notifySuccess = "<p style='color: #fff;'>Фильм успешно добавлен!</p>";
+		} else {
+			$notifyError = "<p style='color: #fff;'>Произошла ошибка! Попробуйте еще раз</p>";
+		}
+	}
+
+}
+
+// Getting films from DB
+$query = "SELECT * FROM films";
+
+$films = array();
+
+$result = mysqli_query($link, $query);
+
+if ( $result = mysqli_query($link, $query) ){
+	while ( $row = mysqli_fetch_array($result) ) {
+	    $films[] = $row;
+	}
+}
+
+ ?>
+
+
+<!-- Разные миксины по одному, которые понадобятся. Для логотипа, бейджа, и т.д.-->
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+	<meta charset="UTF-8" />
+	<title>[Имя и фамилия] - Фильмотека</title>
+	<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"/><![endif]-->
+	<meta name="keywords" content="" />
+	<meta name="description" content="" /><!-- build:cssVendor css/vendor.css -->
+	<link rel="stylesheet" href="libs/normalize-css/normalize.css" />
+	<link rel="stylesheet" href="libs/bootstrap-4-grid/grid.min.css" />
+	<link rel="stylesheet" href="libs/jquery-custom-scrollbar/jquery.custom-scrollbar.css" /><!-- endbuild -->
+	<!-- build:cssCustom css/main.css -->
+	<link rel="stylesheet" href="./css/main.css" /><!-- endbuild -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800&amp;subset=cyrillic-ext" rel="stylesheet">
+	<!--[if lt IE 9]><script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script><![endif]-->
+</head>
+
+<body class="index-page">
+	<div class="container user-content section-page">
+
+		<?php if ( $notifySuccess != '' ) { ?> 
+			<div class="notify notify--success mb-20"><?=$notifySuccess?></div>
+		<?php } ?>
+
+		<?php if ( $notifyError != '' ) { ?> 
+			<div class="notify notify--error mb-20"><?=$notifyError?></div>
+		<?php } ?>
+
+		<div class="title-1">Фильмотека</div>
+
+		<?php 
+
+			foreach ($films as $key => $value) { ?>
+			
+			<div class="card mb-20">
+				<h4 class="title-4"><?=$value['title']?></h4>
+				<div class="badge"><?=$value['genre']?></div>
+				<div class="badge"><?=$value['year']?></div>
+			</div>
+
+		<?php } ?>
+
+		<div class="panel-holder mt-80 mb-40">
+			<div class="title-3 mt-0">Добавить фильм</div>
+			<form action="index.php" method="POST">
+				<?php 
+
+				if ( !empty($errors) ) {
+					foreach ($errors as $key => $value) {
+						echo "<div class='notify notify--error mb-20'>$value</div>";
+					}
+				}
+
+				 ?>
+
+				<div class="form-group"><label class="label">Название фильма<input class="input" name="title" type="text" placeholder="Такси 2" /></label></div>
+				<div class="row">
+					<div class="col">
+						<div class="form-group"><label class="label">Жанр<input class="input" name="genre" type="text" placeholder="комедия" /></label></div>
+					</div>
+					<div class="col">
+						<div class="form-group"><label class="label">Год<input class="input" name="year" type="text" placeholder="2000" /></label></div>
+					</div>
+				</div>
+				<input class="button" type="submit" name="newFilm" value="Добавить" />
+			</form>
+		</div>
+	</div><!-- build:jsLibs js/libs.js -->
+	<script src="libs/jquery/jquery.min.js"></script><!-- endbuild -->
+	<!-- build:jsVendor js/vendor.js -->
+	<script src="libs/jquery-custom-scrollbar/jquery.custom-scrollbar.js"></script>
+	<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAIr67yxxPmnF-xb4JVokCVGgLbPtuqxiA"></script><!-- endbuild -->
+	<!-- build:jsMain js/main.js -->
+	<script src="js/main.js"></script><!-- endbuild -->
+	<script defer="defer" src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+</body>
+
+</html>
