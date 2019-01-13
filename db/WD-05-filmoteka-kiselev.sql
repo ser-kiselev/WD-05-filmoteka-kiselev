4755f21ea84a08789d85915a54f17f6d601059d1
Сергей Киселев <44898506+ser-kiselev@users.noreply.github.com>
2019-01-13 21:07:34 +0300

    made changes

--- db/WD-05-filmoteka-kiselev.sql
+++ db/WD-05-filmoteka-kiselev.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Янв 10 2019 г., 23:55
+-- Время создания: Янв 13 2019 г., 21:06
 -- Версия сервера: 5.5.53
 -- Версия PHP: 5.6.29
 
@@ -38,10 +38,11 @@ CREATE TABLE `films` (
 --
 
 INSERT INTO `films` (`id`, `title`, `genre`, `year`) VALUES
-(1, 'Начало', 'фантастика, боевик', '2010'),
-(2, 'Гладиатор', 'боевик', '2000'),
+(2, 'Гладиатор 2', 'боевик, драма', '2020'),
 (3, 'Игры разума', 'драма', '2001'),
-(4, 'Интерстеллар', 'фантастика', '2014');
+(4, 'Интерстеллар', 'фантастика', '2014'),
+(5, 'Начало', 'фантастика', '2010'),
+(6, 'Миссия невыполнима: Последствия', 'боевик', '2018');
 
 --
 -- Индексы сохранённых таблиц
@@ -61,7 +62,7 @@ ALTER TABLE `films`
 -- AUTO_INCREMENT для таблицы `films`
 --
 ALTER TABLE `films`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
